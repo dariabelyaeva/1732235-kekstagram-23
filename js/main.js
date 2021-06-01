a5d950dd89163ebdfc47d4ccb360be5f456d22d3
dariabelyaeva <79530083+dariabelyaeva@users.noreply.github.com>
2021-06-01 19:04:04 +0300

    Функции доработка №2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 function getRandomNumber(min, max) {
-  if (min < 0 || max < 0 || min >= max) {
-    return null;
+  if (min < 0 || max < 0 || min > max) {
+    throw new Error('Ошибка выбора диапазона');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 getRandomNumber(100, 100);
 
-function checkStringLength(inputString, maxLength) {
-  return (inputString <= maxLength);
+function checkStringLength(string, maxLength) {
+  return (string.length <= maxLength);
 }
-checkStringLength(120, 140);
+checkStringLength('text', 140);
